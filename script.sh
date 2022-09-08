7af3a37c5f9eb0566ffd5a0c0fe1fc3246bcaed2
aavez <root@ip-172-31-0-108.ap-south-1.compute.internal>
2022-09-08 11:18:48 +0000

    uploading the first content of the  day

---
+++ script.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+
+RAM=`free -m | grep Mem | awk '{print $4}'` 
+ 
+LOAD=`uptime | awk '{print $9}'`
+
+
+ROOTFREE=`df -h | grep /dev/xvda2 | awk '{print $4}'`
+
+echo "##############################################"
+echo  "Free ram is $RAM Mb"
+
+echo "##############################################"
+
+echo "Current load average is $LOAD"
+
+echo "##############################################"
+
+echo "Free root partition size is $ROOTFREE"
+
+echo "##############################################"
+
+echo "script is complete and command substitution is done"
+
+
+
